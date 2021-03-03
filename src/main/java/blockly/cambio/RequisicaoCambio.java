2ce9908ed1068da7485ea6f941d6106fa47eb64f
Mariana Van Rossum <mariana.vanrossum@gmail.com>
2021-03-03 19:19:42 +0000

    commit json solucionado

--- src/main/java/blockly/cambio/RequisicaoCambio.java
+++ src/main/java/blockly/cambio/RequisicaoCambio.java
@@ -2,6 +2,7 @@ package blockly.cambio;
 
 import cronapi.*;
 import cronapi.rest.security.CronappSecurity;
+import java.util.Iterator;
 import java.util.concurrent.Callable;
 
 
@@ -26,6 +27,8 @@ public static Var Executar(Var context, Var intents, Var entities) throws Except
    private Var moedaEscolhida = Var.VAR_NULL;
    private Var calculoCambio = Var.VAR_NULL;
    private Var listaCalculoCambio = Var.VAR_NULL;
+   private Var jsonMoeda = Var.VAR_NULL;
+   private Var i = Var.VAR_NULL;
    private Var exception = Var.VAR_NULL;
 
    public Var call() throws Exception {
@@ -59,9 +62,35 @@ public static Var Executar(Var context, Var intents, Var entities) throws Except
         System.out.println(calculoCambio.getObjectAsString());
 
         listaCalculoCambio =
-        cronapi.map.Operations.toList(calculoCambio);
+        cronapi.list.Operations.newList();
 
-        System.out.println(listaCalculoCambio.getObjectAsString());
+        listaCalculoCambio =
+        cronapi.conversion.Operations.convert(
+        cronapi.map.Operations.toList(calculoCambio),
+        Var.valueOf("LIST"));
+
+        jsonMoeda = Var.VAR_NULL;
+
+        for (Iterator it_i = listaCalculoCambio.iterator(); it_i.hasNext();) {
+            i = Var.valueOf(it_i.next());
+
+            if (
+            Var.valueOf(
+            Var.valueOf((
+            cronapi.json.Operations.getJsonOrMapField(i,
+            Var.valueOf("code"))).getObjectAsString().toUpperCase()).equals(
+            Var.valueOf(moedaEscolhida.getObjectAsString().toUpperCase()))).getObjectAsBoolean()) {
+
+                jsonMoeda =
+                cronapi.map.Operations.createObjectMapWith(Var.valueOf("name",
+                cronapi.json.Operations.getJsonOrMapField(i,
+                Var.valueOf("name"))) , Var.valueOf("low",
+                cronapi.json.Operations.getJsonOrMapField(i,
+                Var.valueOf("low"))));
+            }
+        } // end for
+
+        System.out.println(jsonMoeda.getObjectAsString());
      } catch (Exception exception_exception) {
           exception = Var.valueOf(exception_exception);
 
